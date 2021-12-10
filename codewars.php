dbdc2302116e26addb4e02c47fcfdca64f925ba9
PacLord <pachevski@gmail.com>
2021-12-10 18:14:07 +0500

    решил 3 задачи

---
+++ codewars.php
@@ -0,0 +1,33 @@
+<?php
+
+/// https://www.codewars.com/kata/57a0e5c372292dd76d000d7e
+
+function repeatStr($n, $str)
+{
+    $newstr = '';
+    for ($i = 0; $i<$n; $i++){
+        $newstr .= $str;
+    }
+    return $newstr;
+}
+
+/// https://www.codewars.com/kata/5168bb5dfe9a00b126000018/php
+
+function solution($str) {
+    return strrev($str);
+  }
+
+/// https://www.codewars.com/kata/5859c82bd41fc6207900007a/php
+
+function quotable($name, $quote) {
+    return "$name said: \"$quote\"";
+  }
+
+
+/// https://www.codewars.com/kata/582cb0224e56e068d800003c/php
+
+/// https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/php
+
+/// https://www.codewars.com/kata/54ff3102c1bad923760001f3/php
+
+/// https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec/php
