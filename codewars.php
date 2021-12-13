e2732e722125b0caf73d51e2dd1828ba74f24401
PacLord <pachevski@gmail.com>
2021-12-13 16:30:34 +0500

    задачи решены

--- codewars.php
+++ codewars.php
@@ -5,7 +5,7 @@
 function repeatStr($n, $str)
 {
     $newstr = '';
-    for ($i = 0; $i<$n; $i++){
+    for ($i = 0; $i < $n; $i++){
         $newstr .= $str;
     }
     return $newstr;
@@ -26,8 +26,37 @@ function quotable($name, $quote) {
 
 /// https://www.codewars.com/kata/582cb0224e56e068d800003c/php
 
+function litres($t) {
+  $lit = $t * 0.5;
+  return floor($lit);
+}
+
 /// https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/php
+
+function nthEven($n) {
+  return $n * 2 - 2;
+}
 
 /// https://www.codewars.com/kata/54ff3102c1bad923760001f3/php
 
+function getCount($str) {
+  $vowelsCount = 0;
+  $vowels = ['a','e','i','o','u'];
+  for ($i = 0; $i < strlen($str); $i++){
+    if (in_array($str[$i], $vowels)){
+      $vowelsCount++;
+    };
+  }
+  
+  return $vowelsCount;
+}
 /// https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec/php
+
+function persistence(int $num): int {
+  if ($num < 10) return 0;
+  $i = 1;
+  foreach (str_split($num) as $n){
+    $i *= $n;
+  }
+  return 1 + persistence($i);
+}
