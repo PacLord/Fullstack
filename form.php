ee9556ebe073f387e10ecb71cffc065609e90266
PacLord <pachevski@gmail.com>
2021-12-19 16:06:25 +0500

    Занятие 10, создали подключение к базе и форму для внесения данных в базу

---
+++ form.php
@@ -0,0 +1,20 @@
+<?php
+
+$user = 'root';
+$pdo = new Pdo('mysql:dbname=fullstack;host=127.0.0.1;', $user, '');
+
+$name = $_POST['name'];
+$login = $_POST['login'];
+$password = $_POST['password'];
+$city_id = $_POST['city_id'];
+
+$query = "INSERT INTO users (name, login, password, city_id) VALUES(:name, :login, :password, :city_id)";
+$res = $pdo->prepare($query);
+$res->execute([
+    ':name' => $name,
+    ':login' => $login,
+    ':password' => $password,
+    ':city_id' => $city_id,
+]);
+
+header('Location: index.php');
